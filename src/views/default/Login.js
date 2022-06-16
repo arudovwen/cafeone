0988ebe2a8152a10d85e8a51a8af9cdcea4b9b26
arudovwen <successahon@gmail.com>
2022-06-16 22:55:07 +0100

    Update Login.js

--- src/views/default/Login.js
+++ src/views/default/Login.js
@@ -59,11 +59,11 @@ const Login = () => {
             <div className="mb-3 filled form-group tooltip-end-top">
               <CsLineIcons icon="lock-off" />
               <Form.Control type={!showPassword ? 'password' : 'text'} name="password" onChange={handleChange} value={values.password} placeholder="Password" />
-              <span className="position-absolute  e-3 cursor-pointer" size="13" onClick={() => togglePassword()}>
+              <span className="position-absolute  e-3 cursor-pointer" style={{ top: '.7rem' }} color="black" onClick={() => togglePassword()}>
                 {!showPassword ? (
-                  <CsLineIcons icon="eye" className="text-small"  size="13" />
+                  <CsLineIcons icon="eye" className="text-small"  size="14" />
                 ) : (
-                  <CsLineIcons icon="eye-off" className="text-small"  size="13" />
+                  <CsLineIcons icon="eye-off" className="text-small"  size="14" />
                 )}
               </span>
               {errors.password && touched.password && <div className="d-block invalid-tooltip">{errors.password}</div>}
